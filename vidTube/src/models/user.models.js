1638b0675976e54578b204b0d6ae56f6cebd1c9b
Prince Mishra <kmishraprince@gmail.com>
2025-04-16 19:47:06 +0530

    database connection successfull

---
+++ vidTube/src/models/user.models.js
@@ -0,0 +1,13 @@
+/*
+id string pk
+username string
+email string
+fullName string
+avatar string
+coverImage string
+watchHistory ObjectId[] videos
+password string
+refreshToken string
+createdAt Date
+updatedAt Date
+*/
