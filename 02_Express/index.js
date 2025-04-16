86eaed038994e69deb64c7162a426a5acfc3c450
Prince Mishra <kmishraprince@gmail.com>
2025-04-17 01:50:41 +0530

    Access and Refresh Token generated

--- 02_Express/index.js
+++ 02_Express/index.js
@@ -1,15 +1,36 @@
+// require('dotenv').config()
+import 'dotenv/config'
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 //Accept data from frontend side:
 app.use(express.json())
+const morganFormat = ":method :url :status :response-time ms";
 
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 let teaData = []
 let nextId = 1
 
 //add a new tea
 app.post('/teas', (req, res) =>{
+    logger.info("A post request is made to add a new tea")
     const {name, price} = req.body
     const newTea = {id: nextId++, name, price}
     teaData.push(newTea)
