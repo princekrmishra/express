c3bd504934bbee70e4b4a451296d73f833132409
Prince Mishra <kmishraprince@gmail.com>
2025-04-18 00:37:13 +0530

    Added file upload cloudinary

--- vidTube/src/app.js
+++ vidTube/src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -13,6 +14,8 @@ app.use(
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
+
+app.use(cookieParser())
 
 
 //import routes
