e241eef6225e5209f14d4021fe7bdaf3d28cde28
Prince Mishra <kmishraprince@gmail.com>
2025-04-20 01:00:47 +0530

    cloudinary part

--- vidTube/src/routes/healthcheck.routes.js
+++ vidTube/src/routes/healthcheck.routes.js
@@ -3,7 +3,8 @@ import { healthcheck } from "../controllers/healthcheck.controllers.js"
 
 const router = Router()
 // /api/v1/healthcheck/test
+
 router.route("/").get(healthcheck)
-router.route("/test").get(healthcheck)
+
 
 export default router;
